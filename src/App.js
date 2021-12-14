9f614c9f5c3ece0e58e353039f815ed261aa7d35
SarvinTorkaman <88194904+SarvinTorkaman@users.noreply.github.com>
2021-12-14 18:43:33 +0330

    removed some original CSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App container">
-      <header className="App-header"> Hello</header>
+      <header className="App-header"> Dictionary App</header>
     </div>
   );
 }
