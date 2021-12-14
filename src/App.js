3eabe86adbfb31c0689d3ced95cf0c4edaa0c1f4
SarvinTorkaman <88194904+SarvinTorkaman@users.noreply.github.com>
2021-12-14 18:33:11 +0330

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App container">
+      <header className="App-header"> Hello</header>
+    </div>
+  );
+}
+
+export default App;
