d6037908dfe2621684ac0ff881292dbbf13e1578
SarvinTorkaman <88194904+SarvinTorkaman@users.noreply.github.com>
2021-12-22 18:10:29 +0330

    updated the CSS (background color)

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,14 +1,15 @@
 import React from "react";
+import "./Phonetics.css";
 
 export default function Phonetics(props) {
   // console.log(props.phonetics);
 
   return (
-    <div>
+    <div className="Phonetics">
       {props.phonetics.map((phonetic, index) => {
         return (
           <span key={index}>
-            <h5 className=" d-inline ps-1 m-1 mt-3 mb-3">/{phonetic.text}/</h5>
+            <h5 className="  d-inline ps-1 m-1 mt-3 mb-3">/{phonetic.text}/</h5>
           </span>
         );
       })}
